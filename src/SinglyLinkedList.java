5571bea34904f57ab354b6620841aec3b6bdb8c4
mosesbenjamin <mavewreek@gmail.com>
2022-05-01 09:39:23 -0700

    detect a loop in a singly linked list

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -227,15 +227,40 @@ public class SinglyLinkedList {
         temp.next = current.next;
     }
 
+    public boolean containsLoop(){
+        ListNode fastPtr = head;
+        ListNode slowPtr = head;
+        while(fastPtr !=null && fastPtr.next !=null){
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if(slowPtr == fastPtr){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void createALoopInLinkedList(){
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+        ListNode sixth = new ListNode(6);
+
+        head = first;
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = third;
+    }
+
     public static void main(String[] args) {
 
-        SinglyLinkedList someNode = new SinglyLinkedList();
-        someNode.insertFirst(10);
-        someNode.insertFirst(20);
-        someNode.insertFirst(45);
-
-        someNode.display();
-        someNode.deletNode(20);
-        someNode.display();
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.createALoopInLinkedList();
+        System.out.println(singlyLinkedList.containsLoop());
     }
 }

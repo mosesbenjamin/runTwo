2528f2e04f1590c61344a14b7685d88790ea6f57
mosesbenjamin <mavewreek@gmail.com>
2022-05-01 15:56:22 -0700

    check if string is a palindrome

--- src/Arrays/ArrayUtil.java
+++ src/Arrays/ArrayUtil.java
@@ -108,11 +108,25 @@ public class ArrayUtil {
         return sum;
     }
 
+    public boolean isPalindrome(String word){
+        char[] charArray = word.toCharArray();
+        int start = 0;
+        int end = word.length() - 1;
+        while (start < end){
+            if (charArray[start] != charArray[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         ArrayUtil arrayUtil = new ArrayUtil();
         int[] arr = {1,2,3,4,5,7};
         arrayUtil.printArray(arr);
-        int result = arrayUtil.findMissingNumber(arr);
+        boolean result = arrayUtil.isPalindrome("moses");
         System.out.println(result);
     }
 }

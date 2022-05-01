cd2ae7d6349d867124ca500e015ca983ae44a4ef
mosesbenjamin <mavewreek@gmail.com>
2022-04-30 18:52:50 -0700

    Length of a singly linked list

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -17,9 +17,21 @@ public class SinglyLinkedList {
             System.out.print(current.data + "-->");
             current = current.next;
         }
-        System.out.print("null");
+        System.out.println("null");
     }
 
+    public int length(){
+        if(head == null){
+            return 0;
+        }
+        int count = 0;
+        ListNode current = head;
+        while(current !=null){
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
         sll.head = new ListNode(10);
@@ -33,5 +45,6 @@ public class SinglyLinkedList {
         third.next = fourth; // 10 --> 1 -->8 --> 11 --> null
 
         sll.display();
+        System.out.println("Length is " + sll.length());
     }
 }

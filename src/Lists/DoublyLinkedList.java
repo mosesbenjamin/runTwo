297e4269937a0653f5daf16c407fdd6ed8fc6657
mosesbenjamin <mavewreek@gmail.com>
2022-05-01 12:04:59 -0700

    print elements of a doubly linked list

--- src/Lists/DoublyLinkedList.java
+++ src/Lists/DoublyLinkedList.java
@@ -28,4 +28,51 @@ public class DoublyLinkedList {
     public int length(){
         return  length;
     }
+
+    public void insertLast(int value){
+        ListNode newNode = new ListNode(value);
+        if(isEmpty()){
+            head = newNode;
+        }else {
+            tail.next = newNode;
+        }
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+    public void displayForward(){
+        if(head == null){
+            return;
+        }
+        ListNode temp = head;
+        while (temp !=null){
+            System.out.print(temp.data + " ---> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void displayBackwards(){
+        if(tail == null){
+            return;
+        }
+        ListNode temp = tail;
+        while (temp !=null){
+            System.out.print(temp.data + " ---> ");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertLast(1);
+        dll.insertLast(10);
+        dll.insertLast(15);
+        dll.insertLast(25);
+
+        dll.displayForward();
+        dll.displayBackwards();
+    }
 }

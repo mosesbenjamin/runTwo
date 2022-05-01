714ac058708c26505870ca496119690d70c81f24
mosesbenjamin <mavewreek@gmail.com>
2022-05-01 14:57:05 -0700

    reverse an array

--- src/Arrays/ArrayUtil.java
+++ src/Arrays/ArrayUtil.java
@@ -14,7 +14,7 @@ public class ArrayUtil {
         return arr;
     }
 
-    public static int[] removeEvenIntegers(int[] arr){
+    public int[] removeEvenIntegers(int[] arr){
         int n = arr.length;
         int oddCount = 0;
         for (int i = 0; i < n; i++){
@@ -32,9 +32,24 @@ public class ArrayUtil {
         return result;
     }
 
+    public int[] reverseArray(int[] numbers, int start, int end){
+        while(start < end){
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+            start++;
+            end--;
+        }
+        return numbers;
+    }
+
     public static void main(String[] args) {
         ArrayUtil arr = new ArrayUtil();
-        int[] result = arr.removeEvenIntegers(arr.createArray());
+        int[] newArray = arr.createArray();
+        arr.printArray(newArray);
+        int start = 0;
+        int end = newArray.length-1;
+        int[] result = arr.reverseArray(newArray, start, end );
         arr.printArray(result);
     }
 }

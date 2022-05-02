fa5018d0ba15fd9c4ba9fdf2b39f9c0a297c0aaa
mosesbenjamin <mavewreek@gmail.com>
2022-05-02 05:49:43 -0700

    represent a binary search tree

---
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,15 @@
+package BinarySearchTree;
+
+public class BinarySearchTree {
+    private TreeNode root;
+
+    private class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+}

95fcfad6788e85a522926f328f03f1026244e56f
mosesbenjamin <mavewreek@gmail.com>
2022-05-02 06:16:18 -0700

    inset a value in a binary search tree

--- src/BinarySearchTree/BinarySearchTree.java
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package BinarySearchTree;
 
+import BinaryTree.BinaryTree;
+
 public class BinarySearchTree {
     private TreeNode root;
 
@@ -11,5 +13,47 @@ public class BinarySearchTree {
         public TreeNode(int data){
             this.data = data;
         }
+    }
+
+    public void insert(int value){
+        root = insert(root, value);
+    }
+
+    public TreeNode insert(TreeNode root, int value){
+        if(root == null){
+            root = new TreeNode(value);
+            return root;
+        }
+        if(value < root.data){
+            root.left = insert(root.left, value);
+        }else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    public void inOrder(){
+        inOrder(root);
+    }
+
+
+    // nodes will be printed in sorted form with recursive inOrder tree traversal
+    public void inOrder(TreeNode root){
+        if(root ==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(5);
+        bst.insert(3);
+        bst.insert(7);
+        bst.insert(1);
+
+        bst.inOrder();
     }
 }

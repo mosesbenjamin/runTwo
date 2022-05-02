3a0b81fa185e3f9c68a206fe15b41fd9ecd47a89
mosesbenjamin <mavewreek@gmail.com>
2022-05-02 05:33:41 -0700

    level order traversal of a binary tree

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -2,6 +2,9 @@ package BinaryTree;
 
 import com.sun.source.tree.Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
     private TreeNode root;
 
@@ -16,16 +19,21 @@ public class BinaryTree {
     }
 
     public void createBinaryTree(){
-        TreeNode first = new TreeNode(9);
+        TreeNode first = new TreeNode(1);
         TreeNode second = new TreeNode(2);
         TreeNode third = new TreeNode(3);
         TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
+        TreeNode sixth = new TreeNode(6);
+        TreeNode seventh = new TreeNode(7);
 
         root = first;
         first.left = second;
         first.right = third;
-
         second.left = fourth;
+        second.right = fifth;
+        third.left = sixth;
+        third.right = seventh;
     }
 
     public void preOrder(TreeNode root){
@@ -55,9 +63,28 @@ public class BinaryTree {
         System.out.print(root.data + " ");
     }
 
+    public void levelOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            TreeNode temp = queue.poll();
+            System.out.print(temp.data + " ");
+            if(temp.left !=null){
+                queue.offer(temp.left);
+            }
+            if(temp.right !=null){
+                queue.offer(temp.right);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
         bt.createBinaryTree();
-        bt.postOrder(bt.root);
+        bt.levelOrder(bt.root);
     }
 }
